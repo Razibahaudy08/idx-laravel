4e0b74e799151ecb1f1227f49e43028bd96ab364
tegarraysandi2206 <tegarraysandi2206@gmail.com>
2024-07-22 05:44:28 +0000

    first commit

---
+++ resources/views/mahasiswa.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="widht=device-widht,initial-s
+        <title>Daftar Mahasiswa</title>
+</head>
+<body>
+    <h1>DAFTAR MAHASISWA</h1>
+    <o1>
+        <li>Rudi permana</li>
+</body>
+</html>
